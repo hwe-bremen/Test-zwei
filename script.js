a58541fb6045732b58f7dcacde9140ef07d5eaf3
hwe-bremen <43620549+hwe-bremen@users.noreply.github.com>
2020-04-14 21:29:49 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+function isElementInViewport(element) {
+    var rect = element.getBoundingClientRect();
+    return (
+     rect.top >= 0 &&
+     rect.left >= 0 &&
+     rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+   }
+   
+   var elements = document.querySelectorAll(".mein-element, .mein-element-kreis");
+
+    
+   function callbackFunc() {
+    for (var i = 0; i < elements.length; i++) {
+     if (isElementInViewport(elements[i])) {
+    elements[i].classList.add("visible");
+   }
+   
+    /* Else-Bedinung entfernen, um .visible nicht wieder zu löschen, wenn das Element den Viewport verlässt. */
+     else { 
+      elements[i].classList.remove("visible");
+     }
+    }
+   }
+    
+   window.addEventListener("load", callbackFunc);
+   window.addEventListener("scroll", callbackFunc);
+
+ 
+   document.getElementById('button-click').addEventListener('click', elementSichtbar);
+   function elementSichtbar() {   
+    var boxVier = document.getElementById("box4");  
+    boxVier.classList.toggle("sichtbar");
+     }
